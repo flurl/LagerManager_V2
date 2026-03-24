7a68ad12de35afb6630bf1a8632289546a0c3e99
flurl <florian.st.klug@gmail.com>
2026-03-24 18:28:33 +0100

    Add type annotations

--- lagermanager/core/serializers.py
+++ lagermanager/core/serializers.py
@@ -6,10 +6,10 @@ from .models import Period, Workplace
 class PeriodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Period
-        fields = ['id', 'name', 'checkpoint_year', 'start', 'end']
+        fields: list[str] = ['id', 'name', 'checkpoint_year', 'start', 'end']
 
 
 class WorkplaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workplace
-        fields = ['id', 'name', 'default_shift_duration']
+        fields: list[str] = ['id', 'name', 'default_shift_duration']
