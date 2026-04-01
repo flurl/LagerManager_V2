63b747239b5802473357c755d7923d32e773ca50
flurl <florian.st.klug@gmail.com>
2026-04-01 06:11:15 +0200

    Fix non existing icon

--- lager-frontend/src/components/AppShell.vue
+++ lager-frontend/src/components/AppShell.vue
@@ -108,7 +108,7 @@ const navGroups = [
           { to: '/reports/stock-level', icon: 'mdi-chart-line', title: 'Lagerstand' },
           { to: '/reports/current-stock-level', icon: 'mdi-package-variant', title: 'Aktueller Lagerstand' },
           { to: '/reports/inventory', icon: 'mdi-clipboard-text', title: 'Inventur' },
-          { to: '/reports/consumption', icon: 'mdi-chart-area', title: 'Verbrauch' },
+          { to: '/reports/consumption', icon: 'mdi-chart-areaspline', title: 'Verbrauch' },
           { to: '/reports/consumption-totals', icon: 'mdi-sigma', title: 'Gesamtverbrauch' },
           { to: '/reports/total-movements', icon: 'mdi-table', title: 'Gesamte Bewegungen' },
         ],
